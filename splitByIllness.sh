9e5b864f39f3f438079392e97d0f0d110ff4e96c
Wojtek Dabrowski <piotr.dabrowski@htw-berlin.de>
2022-02-08 21:09:08 +0100

    Added progress output

--- splitByIllness.sh
+++ splitByIllness.sh
@@ -1,7 +1,10 @@
 infile="data/example.csv"
 metric="Deaths"
 for illness in "Ebola" "trematodiases" "Leprosy" "Zika"; do
+  echo -n "Splitting out ${metric} data for ${illness}... "
   outfile=${illness}.csv
   head -n 1 ${infile} > ${outfile}
   grep ${illness} ${infile} | grep ${metric} >> ${outfile}
+  lines=`wc -l ${outfile} | cut -d " " -f 1`
+  echo "done, extracted ${lines} entries into ${outfile}."
 done
